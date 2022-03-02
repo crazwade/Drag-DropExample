fc5b2c1b7219a97d40d8de4ea07e93ea42f07554
crazwade <jackywu526@gmail.com>
2022-03-02 16:21:31 +0800

    div 10 -> div 6

--- src/components/DragandDrop.vue
+++ src/components/DragandDrop.vue
@@ -33,7 +33,7 @@ export default {
     const dragDrop = (event) => {
       console.log(`降落: ${event.toElement.id}`);
     };
-    const eventArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const eventArr = [1, 2, 3, 4, 5, 6];
     onMounted(() => {
       const eventDiv = [];
       eventArr.forEach((item) => {
